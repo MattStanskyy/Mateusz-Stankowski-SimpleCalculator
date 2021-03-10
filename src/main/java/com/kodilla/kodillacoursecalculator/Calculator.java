fb770e6e5da9a311d04cb2e554b9e3315e9a0b2e
Mateusz Stankowski <m.stankowski16@gmail.com>
2021-03-10 02:33:03 +0100

    Mateusz-Stankowski-Simple-Calculator

--- src/main/java/com/kodilla/kodillacoursecalculator/Calculator.java
+++ src/main/java/com/kodilla/kodillacoursecalculator/Calculator.java
@@ -1,4 +1,12 @@
 package com.kodilla.kodillacoursecalculator;
 
 public class Calculator {
+
+    public int adding(int x, int y)  {
+        return x + y;
+    }
+
+    public int subtracting(int x, int y) {
+        return x - y;
+    }
 }
--- src/main/java/com/kodilla/kodillacoursecalculator/KodillaCourseCalculatorApplication.java
+++ src/main/java/com/kodilla/kodillacoursecalculator/KodillaCourseCalculatorApplication.java
@@ -8,6 +8,12 @@ public class KodillaCourseCalculatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KodillaCourseCalculatorApplication.class, args);
+
+        Calculator calculator = new Calculator();
+    int resultOfAdding = calculator.adding(10, 5);
+    int resultOfSubtracting = calculator.subtracting(10, 5);
+    System.out.println("Result of adding: " + resultOfAdding);
+        System.out.println("Result of adding: " + resultOfSubtracting);
     }
 
 }
